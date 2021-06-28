c19fc091eead6909dbae4626b0b75ac077d6fef2
Thomas Abel <thomabel@ada.cs.pdx.edu>
2021-06-28 13:55:56 -0700

    almost finished

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ TARGET = domletters
 SRCS = domletters.cpp
 
 $(TARGET): $(SCRS)
-  $(CXX) $(CXXFLAGS) -o $(TARGET) $(SRCS)
+	$(CXX) $(CXXFLAGS) -o $(TARGET) $(SRCS)
 
 clean:
-  -rm -f $(TARGET)
+	-rm -f $(TARGET)
--- domletters.cpp
+++ domletters.cpp
@@ -5,29 +5,51 @@ Date: 2021-06-26
 */
 
 #include <iostream>
+#include <cstring>
 
 const int SIZE = 64;
 
 int analyze(char* word){
-  int total = 0;
+// Analyzes a single word to find the dominant letter count.
+// Uses an array of the 26 lower-case letters to store counts.
+  int alphabet[26];
+  for (int j = 0; j < 26; j++){
+    alphabet[j] = 0;
+  } 
 
-  for (int i = 0; i < strlen(word); i++){
+  for (int i = 0; i < (int)strlen(word); i++){
+    // Make all characters lower-case
     char letter = tolower(word[i]);
+    // Ignore new line characters.
+    if (letter == '\n')
+      continue;
+    // Reject if there are any non-letter characters.
     if (letter < 'a' || letter > 'z'){
       return 0;
     }
+    // Add to array.
+    alphabet[letter - 'a'] += 1;
+  }
+
+  // Find max value from array.
+  int total = 0;
+  for (int j = 0; j < 26; j++){
+    if (alphabet[j] > total)
+      total = alphabet[j];
   }
   return total;
 }
 
 int readinput(){
+// Reads word by word from standard input.
+// Stops when empty string is read.
   int total = 0;
   char* word = new char[SIZE];
 
-  while(!EOF){
+  do {
     std::cin.getline(word, SIZE, ' ');
     total += analyze(word);
-  }  
+  } while(word[0] != '\0');
 
   return total;
 }
