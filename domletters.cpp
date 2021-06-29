7129615205408c22ca47402738b008517595062b
Thomas Abel <thomabel@ada.cs.pdx.edu>
2021-06-28 17:42:39 -0700

    final

--- domletters.cpp
+++ domletters.cpp
@@ -7,6 +7,9 @@ Date: 2021-06-26
 #include <iostream>
 #include <cstring>
 
+// Constant values
+// SIZE is the size of the input line.
+// ABSZ is the size of the character set to be analyzed.
 const int SIZE = 128;
 const int ABSZ = 26;
 
@@ -41,36 +44,24 @@ int analyze(char* word){
 }
 
 int readinput(){
-// Reads from standard input.
+// Reads all text from standard input.
   int total = 0;
   char* line = new char[SIZE];
   char* word;
-  //char peek;
 
+  // Reads by line.
   while (std::cin.getline(line, SIZE)){
+    // Check again for blank lines.
     if (strlen(line) == 0)
       continue;
+    // Tokenize the line into individual words and analyze.
     word = strtok(line, " ");
     while(word != NULL){
       total += analyze(word);
       word = strtok(NULL, " ");
     }
   }
-/*
-  do {
-    // Gets a line from the input.
-    std::cin.getline(line, SIZE);
-    // Breaks it down into separate words.
-    word = strtok(line, " ");
-    while(word != NULL){
-      total += analyze(word);
-      word = strtok(NULL, " ");
-    }
-    // Peek the next character.
-    //peek = std::cin.peek();
-    //std::cout << peek << std::endl;
-  } while(line != "" || cin.good());
-*/
+  // Clean up.
   delete[] line;
   return total;
 }
